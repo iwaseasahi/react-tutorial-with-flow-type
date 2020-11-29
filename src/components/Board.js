eba6078b3dae9347e69a809b89a88e1cdb826680
岩瀬旭 <a-iwase@shikaku-square.com>
2020-11-29 21:55:34 +0900

    Add time travel

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,52 +5,24 @@ import React from 'react';
 import Square from './Square'
 import calculateWinner from '../helpers/CalculateWinner'
 
-type Props = {};
-
-type State = {
+type Props = {
   squares: Array<string | null>,
-  xIsNext: boolean
+  onClick: (i: number) => void
 };
 
-class Board extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true,
-    };
-  }
-
-  handleClick(i: number) {
-    const squares = this.state.squares.slice();
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext,
-    });
-  }
-
+class Board extends React.Component<Props> {
   renderSquare(i: number): React$Element<typeof Square> {
     return (
       <Square
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
       />
     );
   }
 
   render(): React$Element<"div"> {
-    const winner = calculateWinner(this.state.squares);
-    let status;
-    if (winner) {
-      status = 'Winner: ' + winner;
-    } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-    }
-
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
